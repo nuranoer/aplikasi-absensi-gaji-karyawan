d81bfca5821b9f1f501104b92f8f3b1a9c98d441
hanzjefferson <varkhan245@gmail.com>
2025-07-06 14:47:00 +0700

    tiny change

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -4,7 +4,7 @@
             <!-- Logo Header -->
             <div class="logo-header" data-background-color="dark">
                 <a href="index.html" class="logo">
-                    <img src="{{ asset('kaiadmin/assets/img/kaiadmin/logo_light.svg') }}" alt="navbar brand"
+                    <img src="{{ asset('assets/logo/ssy.jpg') }}" alt="navbar brand"
                         class="navbar-brand" height="20" />
                 </a>
                 <div class="nav-toggle">
