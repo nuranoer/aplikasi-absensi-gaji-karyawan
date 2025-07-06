95c6937b6cd100fc23f1e69528c7844ab46744d9
hanzjefferson <varkhan245@gmail.com>
2025-07-06 21:05:11 +0700

    cancel vercel

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,9 @@
 <?php
+
+Route::get('/', function (){
+    return to_route('admin.dashboard');
+});
+
 require __DIR__ . '/auth.php';
 require __DIR__ . '/admin.php';
 require __DIR__ . '/karyawan.php';
